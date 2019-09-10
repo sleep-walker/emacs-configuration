c36f336de2c3201daef6a3e40778f3ca2b845a93
Tomáš Čech <sleep_walker@gnu.org>
2019-09-10 09:46:41 +0200

    add but don't use-package tide

--- .emacs-tcech.el
+++ .emacs-tcech.el
@@ -425,3 +425,31 @@
   (setq server-name (read-string "Server name: "))
   (server-start))
 
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  ;; company is an optional dependency. You have to
+  ;; install it separately via package-install
+  ;; `M-x package-install [ret] company`
+  (company-mode +1))
+
+;; (use-package tide
+;;   :defer t
+;;   :init
+;;   (tide-setup)
+;;   (flycheck-mode +1)
+;;   (setq flycheck-check-syntax-automatically '(save mode-enabled))
+;;   (eldoc-mode +1)
+;;   (tide-hl-identifier-mode +1)
+;;   (setq company-tooltip-align-annotations t)
+;;   (add-hook 'before-save-hook 'tide-format-before-save))
+
+;;  (add-hook 'typescript-mode-hook #'setup-tide-mode)
+
+;; formats the buffer before saving
+
+
